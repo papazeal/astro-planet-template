31f4ae1ca31a746b6407dbf21d90b6371aee9c9f
papazeal <zeally7@gmail.com>
2025-03-28 12:08:23 +0700

    up

--- src/components/CMS/PlanetCMS.svelte
+++ src/components/CMS/PlanetCMS.svelte
@@ -32,10 +32,10 @@
 <Toaster />
 <form method="POST" onsubmit={submit}>
   <div class="shadow-md">
-    <div class="py-4 px-7 flex gap-8">
-      <div class="flex gap-2">
+    <div class="py-4 px-6 flex gap-8 text-lg">
+      <div class="flex gap-2 overflow-hidden items-center">
         <svg
-          class="h-6"
+          class="h-6 shrink-0"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 256 256"
           ><path
@@ -43,9 +43,12 @@
             d="M128 26a102 102 0 1 0 102 102A102.12 102.12 0 0 0 128 26m0 12a89.53 89.53 0 0 1 50 15.2V74a2 2 0 0 1-.48 1.31L155.35 101a2 2 0 0 1-1.25.68L122.63 106a2 2 0 0 1-1.44-.36l-.24-.16l-19.7-12.89a14 14 0 0 0-19.71 3.64l-21 31.3a13.94 13.94 0 0 0-2.36 7.71L58 171.45a2 2 0 0 1-.9 1.66l-5 3.27A90 90 0 0 1 128 38M59.28 186.05l4.43-2.9A14 14 0 0 0 70 171.52l.21-36.23a2.05 2.05 0 0 1 .33-1.1l21-31.3a2 2 0 0 1 1.31-.86a2 2 0 0 1 1.52.35l.24.16l19.7 12.89a13.93 13.93 0 0 0 10 2.44l31.47-4.26a14 14 0 0 0 8.73-4.74l22.17-25.76A14 14 0 0 0 190 74V62.82a89.91 89.91 0 0 1 22.68 95.67l-18.4-16.82a14 14 0 0 0-14.82-2.6L149 151.73a14.11 14.11 0 0 0-8.48 10.89l-2.38 16.19a14 14 0 0 0 10.3 15.58L169.9 200a2.1 2.1 0 0 1 .91.53l4.18 4.18a89.86 89.86 0 0 1-115.71-18.66m125.62 11.63l-5.59-5.61a14 14 0 0 0-6.36-3.65l-21.46-5.63a2 2 0 0 1-1.47-2.23l2.39-16.19a2 2 0 0 1 1.21-1.56l30.45-12.66a2 2 0 0 1 2.12.37l21.36 19.54a90.8 90.8 0 0 1-22.65 27.62"
           /></svg
         >
-        <a href={website}>{website.replace(/(^\w+:|^)\/\//, "")}</a>
+        <a
+          class="whitespace-nowrap overflow-ellipsis overflow-hidden"
+          href={website}>{website.replace(/(^\w+:|^)\/\//, "")}</a
+        >
       </div>
-      <div class="hidden md:flex gap-2">
+      <div class="hidden md:flex gap-2 items-center">
         <svg
           class="h-6"
           xmlns="http://www.w3.org/2000/svg"
@@ -59,7 +62,7 @@
       </div>
       <div class="ml-auto">
         <div
-          class="fixed flex md:relative md:p-0 md:border-0 md:shadow-none bottom-0 left-0 w-full p-4 shadow-lg border-t border-slate-300 gap-4 z-20"
+          class="fixed flex md:relative md:p-0 md:border-0 md:shadow-none bottom-0 left-0 w-full p-4 shadow-xl border-t border-slate-300 gap-4 z-20 bg-white"
         >
           <a
             href="/signout"
@@ -108,7 +111,7 @@
                   type="text"
                   name={group.id + "-" + field.id}
                   value={data[group.id]?.[field.id] || ""}
-                  class="border border-gray-400 px-3 py-1.5 rounded min-w-100 max-w-full"
+                  class="border border-gray-400 px-3 py-1.5 rounded w-100 max-w-full"
                 />
               </div>
             {/each}
